f2eb63fbf83955bfbf9ba2bca6e4c07a072f9a56
AtomicCodeEber <145419360+AtomicCodeEber@users.noreply.github.com>
2023-11-12 20:56:31 -0600

    Add files via upload
    Exposición

---
+++ #1601_Derecho Constitucional.py
@@ -0,0 +1,47 @@
+"Quizz: Derecho Constitucional Interactivo"
+
+print("\n")
+print("Bienvenido al cuestionario sobre el 'Derecho Constitucional' \n")
+
+print("¿Qué es el derecho?\n")
+print("a. Ciencia social que estudia las normas que regulan a la sociedad.")
+print("b. .")
+print("c. Orden normativo que regula la conducta humana.")
+
+laws=input("Respuesta 0: ")
+
+if laws == "c" or "a":
+    print("Correcto :)")
+    r0=1
+else:
+    print("Incorrecto :(")
+    r0=0
+
+print("El Derecho Constitucional es una rama que estudia...\n")
+print("a. La administración pública.")
+print("b. Principios, Conceptos y Leyes.")
+print("c. Las relaciones jurídicas entre individuos.")
+print("d. Las condiciones laborales y económicas.")
+definition=input("Respuesta 1: ")
+
+"Definir cuantas preguntas se realizarán"
+"Implementar un sistema"
+
+if definition == "b":
+    print("Correcto :)")
+    r1=1
+else:
+    print("Incorrecto :(")
+    r1=0
+
+print("¿A qué clasificación pertenece el Derecho Constitucional?\n")
+print("a. Derecho Privado.")
+print("b. Derecho Público.")
+clasification=input("Respuestsa 2: ")
+
+if clasification == "b":
+    print("Correcto :)")
+    r2=1
+else:
+    print("Incorrecto :( ")
+    r2=0
