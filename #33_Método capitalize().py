63e31cb36fba6bcd8c996c2f794acbc1ba395e12
AtomicCodeEber <145419360+AtomicCodeEber@users.noreply.github.com>
2023-11-12 19:36:06 -0600

    Update #33_Método capitalize().py

--- #33_Método capitalize().py
+++ #33_Método capitalize().py
@@ -1,5 +1,19 @@
-
+# This method can write the sentences in capitals in them beggining
 
 name=input("Write your name: ")
 print("Hi, {}, you have to know this".format(name))
 print(name.capitalize())
+
+# Exercise
+
+yeah="yeAH, buDDy"
+print(f"Before correctiom: {yeah}")
+yeah=yeah.capitalize()
+print(f"After correction: {yeah}")
+
+
+"Comparación con 'title()'"
+
+elo=input("Write a name:")
+print(f"Hi, {elo}, this is your name.")
+print(f"This is the correction: {elo.title()}")
