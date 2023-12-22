4f809ee3411b63758dc5476d85c7eb57f19416a8
AtomicCodeEber <145419360+AtomicCodeEber@users.noreply.github.com>
2023-12-21 19:16:05 -0600

    Add files via upload

---
+++ #37_Substrings.py
@@ -0,0 +1,18 @@
+# Subcadenas
+
+hola="0123456789"
+print(hola[0:5])
+print(len(hola))
+print(hola[0:10])
+print(hola[-10:-1])
+print(hola[:])
+print(hola[1:10:2])
+print(hola[::2])
+
+poem="Destellos oscuros, sombras brillantes."
+print(len(poem))
+print(poem[0:10])
+print(poem[-10:-1])
+print(poem[:])
+print(poem[1:10:2])
+print(poem[::2])
