51ab72f301ed0141d62364ca5496366f1089be23
AtomicCodeEber <145419360+AtomicCodeEber@users.noreply.github.com>
2023-12-25 13:11:37 -0600

    Create #38_6th Exercise.py

---
+++ #38_6th Exercise.py
@@ -0,0 +1,14 @@
+"""Desarrollar un programa que elimine una 
+palabra que forme parte de una cadena de caracteres"""
+
+"- La cadena tiene que ser solicitada desde el teclado"
+"- La palabra a eliminar deberá ser solicitada desde teclado"
+
+print("Sistema de erradicación de un caracter en variable string")
+print("\n")
+name=input("Estimado usuario, ingrese su nombre: ")
+print("\n")
+string=input(f"Hola,{name}, ¿Cuál es la frase seleccionada?: ")
+erase=input("¿Cuál es la palabra a eliminar?: ")
+print("\n")
+print(string[:])
